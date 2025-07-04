5c08126f66d6fb3e33e113e45475b604098d0c8a
edgar-mora <edgar.mora@amarello.com.mx>
2025-07-04 17:41:25 -0600

    agregando proyecto

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Saludos a todos desde Node.js!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Node escuchando en puerto ${PORT}`);
+});
+
